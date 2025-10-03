a87bf67617de083cf3cbf7ca41ebcce4788912b5
ridwanalmahmud <ridwanalmahmud107@gmail.com>
2025-10-03 05:02:46 +0000

    bind sys

--- Makefile
+++ Makefile
@@ -2,5 +2,8 @@ all:
 	nasm server.s -o bin/server.o
 	ld server.o -o bin/server
 
+run:
+	@bin/server
+
 clean:
 	rm bin/server*
--- server.s
+++ server.s
@@ -1,3 +1,9 @@
+section .data
+    sockaddr:
+        .family: dw 2
+        .port: dw 0x5000
+        .addr: dd 0
+
 section .text
     global _start
 
@@ -7,6 +13,13 @@ _start:
     mov rdi, 2
     mov rsi, 1
     mov rdx, 0
+    syscall
+
+    ; bind(sock_fd)
+    mov rdi, rax
+    mov rsi, sockaddr
+    mov rdx, 16
+    mov rax, 49
     syscall
 
     ; exit(0)
