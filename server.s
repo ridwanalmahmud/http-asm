8c97f9cf4e9f5c271a1cc1fa3e96ba853e31ab51
ridwanalmahmud <ridwanalmahmud107@gmail.com>
2025-10-03 03:52:23 +0000

    socket sys

--- server.s
+++ server.s
@@ -2,6 +2,14 @@ section .text
     global _start
 
 _start:
+    ; socket(AF_INET, SOCK_STREAM, 0)
+    mov rax, 41
+    mov rdi, 2
+    mov rsi, 1
+    mov rdx, 0
+    syscall
+
+    ; exit(0)
     mov rax, 60
     mov rdi, 0
     syscall
